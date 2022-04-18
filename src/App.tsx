c3d2cc99811bf27b48036f51cf9c0f5a13737fca
Redouane Bouabana <rredouane342@gmail.com>
2022-04-18 19:51:26 +0000

    update all packages

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ function App() {
   console.log(window.ipcRenderer);
 
   return (
-    <div>
-      
+    <div className="h-screen w-screen  flex justify-center items-center">
+      <div className="text-5xl font-extrabold">
+        <span className="bg-clip-text  text-transparent bg-gradient-to-r from-red-500 to-blue-500">
+          Electron * ts * tailwind * vite
+        </span>
+      </div>
     </div>
   );
 }
