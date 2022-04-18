d6071ae534c4615d1ce98612c5384417989b868a
Redouane Bouabana <rredouane342@gmail.com>
2022-04-18 19:59:18 +0000

    update tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   console.log(window.ipcRenderer);
-
+  const [count, setCount] = useState(0);
   return (
-    <div className="h-screen w-screen  flex justify-center items-center">
-      <div className="text-5xl font-extrabold">
-        <span className="bg-clip-text  text-transparent bg-gradient-to-r from-red-500 to-blue-500">
+    <div className="h-screen w-screen  flex justify-center items-center flex-col gap-[5em] ">
+      <div className="">
+        <span className=" text-5xl font-extrabold bg-clip-text  text-transparent bg-gradient-to-r from-red-500 to-blue-500">
           Electron * ts * tailwind * vite
         </span>
+      </div>
+      <div className="text-center flex flex-col">
+        {count}
+        <button
+          className="px-5 py-2 bg-blue-400 m-4 rounded-md font-medium text-white "
+          onClick={() => setCount(count + 1)}
+        >
+          yep
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-	mode: 'jit',
-	purge: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	  ],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {}
